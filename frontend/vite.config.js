e84e8b9d331c3e43425a0440c6e3109079709dee
Deepak M <145912399+Deepak-dev46@users.noreply.github.com>
2026-08-05 22:34:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react],
   server: {
-    port: 5173
+    host: '0.0.0.0',
+    allowedHosts: [
+      'ec2-34-239-245-63.compute-1.amazonaws.com'
+    ]
   }
 })
