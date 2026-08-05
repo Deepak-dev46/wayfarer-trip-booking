2a6d023b85e94804cda5d2dd81f4ea45fb60846c
Deepak M <145912399+Deepak-dev46@users.noreply.github.com>
2026-08-05 23:35:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: [
-      'http://ec2-34-202-124-205.compute-1.amazonaws.com',
-      'http://ec2-34-239-245-63.compute-1.amazonaws.com'
-    ]
+    allowedHosts: true
   }
 })
